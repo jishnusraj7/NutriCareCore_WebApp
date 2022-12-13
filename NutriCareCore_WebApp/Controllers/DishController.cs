54c7e0d785dd7f429ef56e9dd7e36138cc03f64f
jishnusraj7 <jishnu.sraj7@gmail.com>
2022-12-13 10:12:49 +0530

    13-Dec-2022

---
+++ NutriCareCore_WebApp/Controllers/DishController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NutriCareCore_WebApp.Data;
+using NutriCareCore_WebApp.Models;
+
+namespace NutriCareCore_WebApp.Controllers
+{
+    public class DishController : Controller
+    {
+        private readonly AppDbContext _db;
+        public IEnumerable<Dish> Dishes { get; set; }
+        [BindProperty]
+        public Dish Dish { get; set; }
+        public DishController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var obj_list = _db.Dish.ToList();
+            return View(obj_list);
+        }
+
+        public IActionResult AddDish()
+        {
+            var obj_list = _db.Dish.ToList();
+            return View(obj_list);
+        }
+    }
+}
---
+++ NutriCareCore_WebApp/Models/Dish.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace NutriCareCore_WebApp.Models
+{
+    public class Dish
+    {
+        [Key]
+        public int DishId { get; set; }
+        [Required]
+        public string DishName { get; set; }
+        [Required]
+        public string DisplayImage { get; set; }
+ 
+    }
+}
